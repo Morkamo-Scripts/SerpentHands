1bbd57daaf5e7cf430f4bbbb5f8f61e38c40a2f4
Кирилл <131317714+Morkamo@users.noreply.github.com>
2025-12-27 13:00:21 +0900

    Initial commit

---
+++ SerpentHands/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Exiled.API.Features;
+using HarmonyLib;
+
+namespace SerpentHands
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => nameof(SerpentHands);
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 12, 1);
+
+        public static Plugin Instance { get; private set; }
+        private static Harmony _harmony;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            _harmony = new Harmony("ru.morkamo.serpentHands.patches");
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            _harmony = null;
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
